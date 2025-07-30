5c362eb8395b3aa761a9386502efaba1c5a32246
JKW1008 <todendvv91@gmail.com>
2025-07-30 20:23:24 +0900

    [Feat] Text Interpolation

--- v02/src/App.vue
+++ v02/src/App.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<script>
+export default {
+  data: function () {
+    return {
+      channel: "왕초보 홈페이지 만들기",
+      subscribe: 2677,
+    };
+  },
+};
+</script>
 
-<template></template>
+<template>
+  <h1>채널명 : {{ channel }}</h1>
+  <h2>구독자수 : {{ subscribe }}</h2>
+  <p>오늘의 행운 숫자 : {{ Math.ceil(Math.random() * 45) }}</p>
+</template>
 
 <style scoped></style>
