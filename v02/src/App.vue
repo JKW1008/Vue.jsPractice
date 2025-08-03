058e98ac744426a216dbc42603a59cbd86a5061b
JKW1008 <todendvv91@gmail.com>
2025-08-03 19:04:21 +0900

    [Study] directive v-pre, v-html, v-text

--- v02/src/App.vue
+++ v02/src/App.vue
@@ -2,7 +2,7 @@
 export default {
   data: function () {
     return {
-      channel: "왕초보 홈페이지 만들기",
+      channel: "<b>왕초보 홈페이지 만들기</b>",
       subscribe: 2677,
     };
   },
@@ -10,9 +10,11 @@ export default {
 </script>
 
 <template>
-  <h1>채널명 : {{ channel }}</h1>
+  <h1 v-pre>채널명 : {{ channel }}</h1>
   <h2>구독자수 : {{ subscribe }}</h2>
   <p>오늘의 행운 숫자 : {{ Math.ceil(Math.random() * 45) }}</p>
+  <p v-html="channel"></p>
+  <h2 v-text="subscribe"></h2>
 </template>
 
 <style scoped></style>
