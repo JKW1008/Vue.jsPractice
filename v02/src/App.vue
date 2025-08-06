72209a688b3feedc46012a1be657744afcdf7fe7
JKW1008 <todendvv91@gmail.com>
2025-08-06 17:21:01 +0900

    [Study] v-model with v-for

--- v02/src/App.vue
+++ v02/src/App.vue
@@ -2,17 +2,45 @@
 export default {
   data() {
     return {
-      agree: false,
+      itemName: null,
+      itemCnt: 1,
+      sList: [{ name: "감귤", cnt: 2 }],
     };
+  },
+  methods: {
+    addItem() {
+      let item = {
+        name: this.itemName,
+        cnt: this.itemCnt,
+      };
+      this.sList.push(item);
+      this.itemName = null;
+      this.itemCnt = 1;
+    },
   },
 };
 </script>
 
 <template>
-  <p>이 약관에 동의하십니까?</p>
-  <input type="checkbox" v-model="agree" />
-  {{ agree }}
-  <button v-show="agree">가입하기</button>
+  <form v-on:submit.prevent="addItem">
+    <p>품목 : <input type="text" required v-model="itemName" /></p>
+    <p>
+      수량 :
+      <select v-model="itemCnt">
+        <option value="1">1개</option>
+        <option value="2">2개</option>
+        <option value="3">3개</option>
+        <option value="4">4개</option>
+        <option value="5">5개</option>
+      </select>
+    </p>
+    <button>상품추가</button>
+    <p>{{ itemName }} {{ itemCnt }}</p>
+  </form>
+  <p>구입할 품목 :</p>
+  <ul>
+    <li v-for="item in sList">{{ item.name }} ({{ item.cnt }}개)</li>
+  </ul>
 </template>
 
 <style></style>
