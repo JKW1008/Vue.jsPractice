415034ae81d2379e61d05a5edfccc02494c09103
JKW1008 <todendvv91@gmail.com>
2025-08-06 12:18:51 +0900

    [Study] v-for

--- v02/src/App.vue
+++ v02/src/App.vue
@@ -1,22 +1,17 @@
 <script>
 export default {
-  data: function () {
+  data() {
     return {
-      score: 55,
-      subscribe: 2677,
+      sports: ["Baseball", "Football", "Volleyball", "Swimming"],
     };
   },
 };
 </script>
 
 <template>
-  <!-- <h1 v-if="score >= 90">평점 : A</h1>
-  <h1 v-else-if="score >= 80">평점 : B</h1>
-  <h1 v-else-if="score >= 70">평점 : C</h1>
-  <h1 v-else>평점 : F</h1> -->
-
-  <h1 v-if="score != 60">이 영역은 v-if에서 처리하고 있습니다.</h1>
-  <h1 v-show="score != 60">이 영역은 v-show에서 처리하고 있습니다.</h1>
+  <ul>
+    <li v-for="sport in sports">{{ sport }}</li>
+  </ul>
 </template>
 
 <style></style>
