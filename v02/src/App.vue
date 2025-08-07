fe3a61e065b694be6f4a4c15e0702760a2bbac17
JKW1008 <todendvv91@gmail.com>
2025-08-07 18:14:17 +0900

    [Study] compute binding

--- v02/src/App.vue
+++ v02/src/App.vue
@@ -1,24 +1,33 @@
+<template>
+  <div>
+    <p>Full Name: {{ fullName }}</p>
+    <input v-model="fullName" placeholder="Enter full name" />
+    firstName : {{ firstName }} <br />
+    lastNam : {{ lastName }}
+  </div>
+</template>
+
 <script>
 export default {
   data() {
     return {
-      chk: false,
+      firstName: "John",
+      lastName: "Doe",
     };
   },
   computed: {
-    yesNo() {
-      if (this.chk) {
-        return "yes";
-      } else {
-        return "no";
-      }
+    fullName: {
+      get() {
+        return this.firstName + " " + this.lastName;
+      },
+      set(newValue) {
+        const names = newValue.split(" ");
+        this.firstName = names[0];
+        this.lastName = names[names.length - 1];
+      },
     },
   },
 };
 </script>
-
-<template>
-  <input type="checkbox" v-model="chk" /> {{ yesNo }} {{ yesNo }} {{ yesNo }}
-</template>
 
 <style></style>
