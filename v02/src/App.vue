0c08b8e98f21fb800d3e4f0437bc63f9f503cd00
JKW1008 <todendvv91@gmail.com>
2025-08-07 19:48:54 +0900

    [Study] watcher

--- v02/src/App.vue
+++ v02/src/App.vue
@@ -1,33 +1,11 @@
-<template>
-  <div>
-    <p>Full Name: {{ fullName }}</p>
-    <input v-model="fullName" placeholder="Enter full name" />
-    firstName : {{ firstName }} <br />
-    lastNam : {{ lastName }}
-  </div>
-</template>
-
 <script>
 export default {
-  data() {
+  datA() {
     return {
-      firstName: "John",
-      lastName: "Doe",
+      count: 0,
     };
-  },
-  computed: {
-    fullName: {
-      get() {
-        return this.firstName + " " + this.lastName;
-      },
-      set(newValue) {
-        const names = newValue.split(" ");
-        this.firstName = names[0];
-        this.lastName = names[names.length - 1];
-      },
-    },
   },
 };
 </script>
-
+<templates></templates>
 <style></style>
