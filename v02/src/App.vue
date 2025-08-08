13d3524c4d1fa781e113ee343f9b2745e477532c
JKW1008 <todendvv91@gmail.com>
2025-08-08 21:53:21 +0900

    [Study] Vue LifCycle and Hook

--- v02/src/App.vue
+++ v02/src/App.vue
@@ -1,14 +1,48 @@
 <script>
-import LocalComponent from "./components/LocalChild.vue";
+import GlobalChild from "./components/GlobalChild.vue";
+
 export default {
-  components: {
-    LocalComponent,
+  data() {
+    return {
+      cnt: 1,
+    };
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount");
+  },
+  unmounted() {
+    console.log("unmounted");
+  },
+  methods: {
+    counter() {
+      this.cnt++;
+    },
+    discounter() {
+      this.cnt--;
+    },
   },
 };
 </script>
 <template>
   <h1>App.Vue</h1>
-  <LocalComponent />
-  <GlobalChild />
+  <h1>Count : {{ cnt }}</h1>
+  <GlobalChild v-if="cnt == 2" />
+  <button @click="counter">Count + 1</button>
+  <button @click="discounter">Count - 1</button>
 </template>
 <style></style>
--- v02/src/components/GlobalChild.vue
+++ v02/src/components/GlobalChild.vue
@@ -1,3 +1,29 @@
+<script>
+export default {
+  beforeCreate() {
+    console.log("GlobalComponent beforeCreate");
+  },
+  created() {
+    console.log("GlobalComponent created");
+  },
+  beforeMount() {
+    console.log("GlobalComponent beforeMount");
+  },
+  mounted() {
+    console.log("GlobalComponent mounted");
+  },
+  updated() {
+    console.log("GlobalComponent updated");
+  },
+  beforeUnmount() {
+    console.log("GlobalComponent beforeUnmount");
+  },
+  unmounted() {
+    console.log("GlobalComponent unmounted");
+  },
+};
+</script>
+
 <template>
   <h1>Global Component</h1>
   <p>it's Global Component</p>
