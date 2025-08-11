e6eed5ac0a76b0df175fd6dec26ba1c016c84507
JKW1008 <todendvv91@gmail.com>
2025-08-11 17:49:48 +0900

    [Stduy] emit

---
+++ v05/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import LocalChild from "@/LocalChild.vue";
+export default {
+  data() {
+    return {
+      c: 0,
+    };
+  },
+  components: {
+    LocalChild,
+  },
+  methods: {
+    callFunction(var1) {
+      console.log("Event called from child component");
+      console.log("child component : " + var1);
+      this.c = var1;
+    },
+  },
+};
+</script>
+<template>
+  <h1>I'm parent compontent. ( {{ c }} )</h1>
+  <LocalChild @create-post="callFunction" />
+</template>
+<style></style>
---
+++ v05/src/LocalChild.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      c: 5,
+    };
+  },
+  emits: ["createPost"],
+};
+</script>
+
+<template>
+  I'm Child component ( {{ c }} )
+  <input type="text" ref="myInput" v-model="c" />
+  <button @click="$emit('createPost', c)">Call parent's event</button>
+</template>
