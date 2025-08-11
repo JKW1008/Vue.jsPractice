0d2699dc63ce93429ac0a6f3c94c653006991f7b
JKW1008 <todendvv91@gmail.com>
2025-08-11 17:50:25 +0900

    [Study] refac

---
+++ v04/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import ChildCom from "./ChildComponent.vue";
+export default {
+  data() {
+    return {
+      name: "John",
+      age: 30,
+    };
+  },
+  components: {
+    ChildCom,
+  },
+  methods: {
+    ageInc() {
+      this.age++;
+    },
+  },
+};
+</script>
+<template>
+  <h1>I'm parents</h1>
+  <button @click="ageInc">age ++</button>
+  <ChildCom v-bind:name="name" v-bind:age="age" />
+</template>
+<style></style>
