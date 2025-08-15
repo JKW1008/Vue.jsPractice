ccd365236cc6717e17f9787d63f3e3e7c091f0d3
JKW1008 <todendvv91@gmail.com>
2025-08-15 20:31:53 +0900

    [Study] ref, reactive

---
+++ Ref/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { reactive, ref } from "vue";
+
+const count = ref(0);
+
+const obj1 = reactive({
+  name: "kim",
+  age: 33,
+});
+
+const good = () => {
+  // count.value++;
+  obj1.name = "lee";
+};
+</script>
+<template>
+  <p>count : {{ count }}</p>
+  <p>obj1.name : {{ obj1.name }}</p>
+  <p>obj1.age : {{ obj1.age }}</p>
+  <button @click="good">click</button>
+</template>
