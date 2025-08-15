7e3313cb17ec4e103a182c3d63a1f73dd8804f96
JKW1008 <todendvv91@gmail.com>
2025-08-15 17:41:50 +0900

    [Study] todo list

---
+++ todo/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import TodoInput from "@/components/TodoInput.vue";
+import TodoList from "@/components/TodoList.vue";
+
+export default {
+  components: {
+    TodoInput,
+    TodoList,
+  },
+};
+</script>
+<template>
+  <div class="todo-container">
+    <h1>TODO List</h1>
+    <TodoInput />
+    <TodoList />
+  </div>
+</template>
---
+++ todo/src/components/TodoInput.vue
@@ -0,0 +1,6 @@
+<template>
+  <form id="todo-form">
+    <input type="text" id="todo-input" placeholder="Enter a new task" />
+    <button type="button">Add</button>
+  </form>
+</template>
