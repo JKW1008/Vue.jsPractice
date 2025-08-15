16b9190c6519f817d380391bb8b0ded49cac7f00
JKW1008 <todendvv91@gmail.com>
2025-08-15 17:52:29 +0900

    [Study] todo list # 2

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,18 +1,36 @@
 <script>
 import TodoInput from "@/components/TodoInput.vue";
 import TodoList from "@/components/TodoList.vue";
+import { v4 as uuidv4 } from "uuid";
 
 export default {
+  data() {
+    return {
+      todoList: [],
+    };
+  },
   components: {
     TodoInput,
     TodoList,
+  },
+  methods: {
+    todoInput(msg) {
+      const item = {
+        id: uuidv4(),
+        msg: msg,
+        completed: false,
+      };
+      // console.log(item);
+      this.todoList.push(item);
+      console.log(JSON.stringify(this.todoList));
+    },
   },
 };
 </script>
 <template>
   <div class="todo-container">
     <h1>TODO List</h1>
-    <TodoInput />
+    <TodoInput @todo-input="todoInput" />
     <TodoList />
   </div>
 </template>
