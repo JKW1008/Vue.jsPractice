74969d798337f2c6f5573ff5763287a5feb59cb1
JKW1008 <todendvv91@gmail.com>
2025-08-18 20:52:12 +0900

    [Study] Get json data with axios

--- AXIOS/src/App.vue
+++ AXIOS/src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
 import axios from "axios";
+import { ref } from "vue";
+
+const table = ref(null);
 
 const inputData = () => {
   axios
@@ -15,7 +18,39 @@ const inputData = () => {
     });
   console.log("Hello");
 };
+
+const getData = () => {
+  axios.get("http://localhost:3000/member").then((res) => {
+    // console.log(res);
+    const members = res.data;
+    let htmlContent = `<table border = "1">
+      <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Email</th>
+      </tr>`;
+    members.forEach((member) => {
+      htmlContent += `
+      <tr>
+        <td>${member.id}</td>
+        <td>${member.name}</td>
+        <td>${member.email}</td>
+      </tr>
+      `;
+    });
+    htmlContent += `</table>`;
+    table.value.innerHTML = htmlContent;
+  });
+};
 </script>
 <template>
   <button @click="inputData">Input data</button>
+  <button @click="getData">Get data</button>
+
+  <div ref="table"></div>
 </template>
+<style scoped>
+div {
+  margin-top: 10px;
+}
+</style>
