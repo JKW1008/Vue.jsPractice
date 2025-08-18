6c10fb13ba9c4541449d2e35fbc4c545e2c1f341
JKW1008 <todendvv91@gmail.com>
2025-08-18 17:45:42 +0900

    [Study] Axios

---
+++ AXIOS/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import axios from "axios";
+
+const inputData = () => {
+  axios
+    .post("http://localhost:3000/member", {
+      name: "lee",
+      email: "lee@gmail.com",
+    })
+    .then((res) => {
+      console.log("complete");
+    })
+    .catch((err) => {
+      console.log("server has error");
+    });
+  console.log("Hello");
+};
+</script>
+<template>
+  <button @click="inputData">Input data</button>
+</template>
