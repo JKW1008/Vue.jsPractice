6fac09e3778238651e18724be3d711d146705e48
JKW1008 <todendvv91@gmail.com>
2025-08-18 17:23:02 +0900

    [Study] defineEmits with Composition API

---
+++ CLS/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import ChildComponent from "@/components/ChildComponent.vue";
+
+const handleCustomEvent = (message) => {
+  console.log("Received event: " + message);
+};
+</script>
+<template><ChildComponent @custom-event="handleCustomEvent" /></template>
---
+++ CLS/src/components/ChildComponent.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { defineEmits } from "vue";
+
+const emit = defineEmits(["customEvent"]);
+const handleClick = () => {
+  emit("customEvent", "Button clicked!");
+};
+</script>
+<template>
+  <button @click="handleClick">Click on me</button>
+</template>
