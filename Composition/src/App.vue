2d7d6a9a96087e99d746b027d4a22a1adadab696
JKW1008 <todendvv91@gmail.com>
2025-08-18 16:50:13 +0900

    [Study] Composition API, defineProps

--- Composition/src/App.vue
+++ Composition/src/App.vue
@@ -1,20 +1,13 @@
 <script setup>
-import { ref, watchEffect } from "vue";
+import ButtonCounter from "@/components/ButtonCounter.vue";
 
-const count = ref(1);
-const name = ref("Jason");
-const counter = () => count.value++;
-const updateName = () =>
-  (name.value = name.value == "Jason" ? "Mike" : "Jason");
-
-watchEffect(() => {
-  console.log(`Count is ${count.value}. The name was ${name.value}`);
-});
+const post = {
+  id: 1,
+  title: "This is title",
+  mainCont: "This is main content",
+};
 </script>
 <template>
-  <p>watchEffect</p>
-  <p>count : {{ count }}</p>
-  <p>name : {{ name }}</p>
-  <button @click="counter">Click</button>
-  <button @click="updateName">Change Name</button>
+  <h1>The components below are child components.</h1>
+  <ButtonCounter :title="post.title" :mainCont="post.mainCont" />
 </template>
